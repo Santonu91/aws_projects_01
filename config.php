9e5c07fd2a0f34798a691c4914d8f47b720de46f
Santonu91 <91974759+Santonu91@users.noreply.github.com>
2024-04-12 18:33:07 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Database connection parameters
+$servername = "your_rds_mysql_host";
+$username = "your_rds_mysql_username";
+$password = "your_rds_mysql_password";
+$dbname = "your_rds_mysql_database";
+
+// Table name
+$table_name = "your_table_name";
+?>
